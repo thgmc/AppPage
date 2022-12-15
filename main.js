60b330fd4e9ad079f8e55f4b49d5731a2050164a
ThiagoMorenoCordeiro <cordeiro.thiago@hotmail.com>
2022-12-15 17:03:17 -0300

    second commit

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function criaLinha(bcdata){
 function main(){
     res = fazGet('https://api.bcb.gov.br/dados/serie/bcdata.sgs.12/dados/ultimos/10?formato=json')
     let dados = JSON.parse(res)
-    let tabela = document.getElementById('tabela')*/
+    let tabela = document.getElementById('tabela')
     //console.log("Hello World!")
 
     dados.array.forEach(element => {
