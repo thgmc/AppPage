33623d951d1e39c81a5690cee5f3522af01d3e65
ThiagoMorenoCordeiro <cordeiro.thiago@hotmail.com>
2022-12-15 16:56:56 -0300

    console.log(dados)

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function main(){
     res = fazGet('https://api.bcb.gov.br/dados/serie/bcdata.sgs.12/dados/ultimos/10?formato=json')
     let dados = JSON.parse(res)
     let tabela = document.getElementById('tabela')
-    sconsole.log(dados)
+    console.log(dados)
 
     /*dados.array.forEach(element => {
         let linha = criaLinha(element)
