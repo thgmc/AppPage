d475ea095da00981f8e0e953ca8f3a99195d3b7c
ThiagoMorenoCordeiro <cordeiro.thiago@hotmail.com>
2022-12-15 16:10:26 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function fazGet(url){
+    let request = new XMLHttpRequest()
+    request.open("GET", url, false)
+    request.send()
+    return request.responseText
+}
+
+function main(){
+    console.log(fazGet('https://api.bcb.gov.br/dados/serie/bcdata.sgs.20542/dados/ultimos/10?formato=json'))
+}
+
+main()
